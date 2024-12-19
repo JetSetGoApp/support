cb4eaffd6102d01caf8e0753175c751211f91c1c
mohamed-ibrahem <mohamed-ibrahem@users.noreply.github.com>
2024-12-19 10:09:30 +0000

    Dusting

--- src/Contracts/Suppliers/HotelData.php
+++ src/Contracts/Suppliers/HotelData.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace JetSetGo\Support\Contracts\Suppliers;
 
 use Spatie\LaravelData\Data;
--- src/Contracts/Suppliers/SearchDate.php
+++ src/Contracts/Suppliers/SearchDate.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace JetSetGo\Support\Contracts\Suppliers;
 
 use Illuminate\Support\Carbon;
